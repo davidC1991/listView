6a72e9b04e2b058f1d1112b9b962df48b2a76a99
jesus callejas <david.callejasc@hotmail.es>
2020-06-10 15:59:32 -0500

    pantalla de areas y volumenes ok

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView titulo;
+    private ListView listView;
 
+    private String opciones[]={"AREAS","VOLUMENES","OPERACIONES REALIZADAS"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        titulo=(TextView) findViewById(R.id.lblTituloListView);
+        listView=(ListView) findViewById(R.id.lvPantalla1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,opciones);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                titulo.setText("la opcion escogida es" + listView.getItemAtPosition(position));
+            }
+        });
     }
 }
